830a1d95ce2f170000a6e7a6b929f7db0b4d621a
Jayanta Mahato <jayantamahato830@gmail.com>
2023-07-14 20:42:49 +0530

    added

--- index.js
+++ index.js
@@ -2,25 +2,35 @@ import Express from "express";
 import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
 const app = Express();
+const API_KEY = "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";
 const port = 3001;
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 app.use(cors());
 app.use(Express.json());
 app.post("/", async (req, res) => {
-  const data = req.body.message;
+  const message = req.body.message;
+  const temperature = req.body.temperature;
+  const model = req.body.model;
   const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: data,
-    max_tokens: 100,
-    temperature: 0.5,
+    model: model,
+    prompt: message,
+    max_tokens: 16,
+    temperature: temperature,
   });
-  console.log("Me:", data);
 
-  console.log("Response:", response.data.choices[0].text);
   res.send(response.data.choices[0].text);
+});
+
+app.get("/models", async (req, res) => {
+  const configuration = new Configuration({
+    apiKey: API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const response = await openai.listModels();
+  res.send(response.data["data"]);
 });
 
 app.listen(port, () => {
